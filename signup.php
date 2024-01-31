1f23b6cbfb574b25a7add4286e9397a229bd0fe8
dungnguyen05052003 <manhdung12b7.2003@gmail.com>
2024-01-31 15:57:17 +0700

    init project

---
+++ signup.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <title>Home page Login System</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="home index">
+    <div class="wrap">
+        <nav class="nav nav custom">
+            <h1>LOGIN</h1>
+            <form action="" class="form-signin">
+                <input type="text" placeholder="Username/Email" name="user_uid">
+                <input type="password" placeholder="Password" name="user_pwd">
+                <button type="submit">Login</button>
+            </form>
+            <a href="signup.php" class="btn btn-red signup">Register</a>
+        </nav>
+        <section>
+                <h3>Sign up</h3>
+                <div class="content">
+                <form action="">
+                    <input type="text" placeholder="firstname" name="user_first">
+                    <input type="text" placeholder="lastname" name="user_last">
+                    <input type="email" placeholder="Email..." name="user_email">
+                    <input type="text" placeholder="username" name="user_uid">
+                    <input type="password" placeholder="password" name="user_pwd">
+                    <button type="submit" class="btn btn-submit">Register</button>
+                </form>
+                </div>
+        </section>
+        <footer>
+            <p>Design by <a href="#">Mạnh Dũng</a></p>
+        </footer>
+    </div>
+</body>
+</html>
